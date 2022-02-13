0073093a31a2d85d6c83562b6c0fe1932c3f975e
jwellik <jwellik@usgs.gov>
2022-02-13 12:11:02 -0800

    init commit

--- utils.py
+++ utils.py
@@ -1,10 +1,10 @@
 def grid_size(img_size, N):
-'''
-Determine the grid size for an image of h pixels x w pixels given N divisions
-N is automatically rounded up to the largest number to complete the rectangle
- img_size : tuple : width x height
- N : int : desired number of squares
-'''
+#'''
+#Determine the grid size for an image of h pixels x w pixels given N divisions
+#N is automatically rounded up to the largest number to complete the rectangle
+# img_size : tuple : width x height
+# N : int : desired number of squares
+#'''
 	import math
 	w = img_size[0]
 	h = img_size[1]
@@ -19,7 +19,7 @@ N is automatically rounded up to the largest number to complete the rectangle
 	else:
 		print('Landscape')
 		grid = ( max(d1,d2), min(d1,d2) )
-	print('Cell size: {}-by-{}, {}x{} --> {}x{}'.format(s, s, grid[0],grid[1], s*grid[0],s*grid[1]))
+	print('Cell size: {}-by-{} px, {}x{} grid --> {}x{} px'.format(s, s, grid[0],grid[1], s*grid[0],s*grid[1]))
 	return s, grid
 
 
@@ -37,14 +37,25 @@ def chop_file(filepath, N):
 	#os.chdir(dir)
 
 	# Save Chops of original image
+	col = "A"
+	row = 1
 	for x0 in range(0, width, chopsize):
+		row = 1
 		for y0 in range(0, height, chopsize):
 			box = (x0, y0,
 				x0+chopsize if x0+chopsize <  width else  width - 1,
 				y0+chopsize if y0+chopsize < height else height - 1)
-			print('{} {}'.format(infile, box))
-			#img.crop(box).save('{}_x{}-y{}.jpg'.format(dir, x0, y0))
-			img.crop(box).save('{}_x{}-y{}.jpg'.format(infile.replace('.jpg',''), x0, y0))
+			#print('{} {}'.format(infile, box))
+			cell_filename = '{}_{}_x{}-y{}.jpg'.format(
+				infile.replace('.jpg',''),
+				"{}{}".format(col, row),
+				x0, y0,
+				)
+			img.crop(box).save(cell_filename)
+			#print("{}{}".format(col, row))
+			print(cell_filename)
+			row += 1
+		col = chr(ord(col)+1).capitalize()
 	#os.chdir('..')
 	draw_grid(img, grid, chopsize, infile)
 
@@ -60,10 +71,17 @@ def draw_grid(im, grid, sz, outpath):
 	import os
 	
 	draw = ImageDraw.Draw(im)
+	col = "A"
+	row = 1
 	for y in range(0,grid[1]+1): # draw horizontal lines
+		col = "A"
 		draw.line((0,y*sz,grid[0]*sz,y*sz), fill=128)
 		for x in range(0, grid[0]+1):
-			draw.text( (x*sz, y*sz), '({}x{})'.format(x*sz, y*sz) )
+			draw.text( (x*sz, y*sz), "{} ({}x{})".format(
+				"{}{}".format(col, row),
+				x*sz, y*sz))
+			col = chr(ord(col)+1)
+		row += 1
 	for x in range(0, grid[0]+1): # draw vertical lines
 		draw.line((x*sz,0,x*sz,grid[1]*sz), fill = 128)
 	im.save('{}_grid.jpg'.format(outpath.replace('.jpg','')))
